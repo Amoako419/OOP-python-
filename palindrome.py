bdd3ac658c01654e96b9fad9f02c8da57a779f99
amoako419 <ghheskey@gmail.com>
2024-04-23 10:20:10 +0000

    initial commit

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,17 @@
-def solution(inputString):
-    return inputString == inputString[::-1]
+# def solution(inputString):
+#     return inputString == inputString[::-1]
 
 
+# letters = "abcdefghijklmnopqrstuvwxyz"
 
+# print(letters[-1::-1])
 
-
+inputArray = [3,6,-2,-5,7,3]
+# def solution(inputArray):
+#     inputArray.sort()
+#     print(inputArray())
+   
+   
+# solution([3,6,-2,-5,7,3])
+inputArray.sort()
+print(inputArray)
