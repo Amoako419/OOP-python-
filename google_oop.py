4ed4a64e5531eb61aef0b03c03d9b004eecebd9f
amoako419 <ghheskey@gmail.com>
2024-09-18 04:31:29 +0000

    piggy years calculated

--- google_oop.py
+++ google_oop.py
@@ -10,3 +10,32 @@ jonagold.color = "red"
 
 print(jonagold.color.upper())
 print(jonagold.flavor.capitalize())
+
+
+################################################################
+
+class Piglet:
+    name = "piglet"
+    def speak(self):
+        print("Oink! I'm {}! Oink!". format(self.name))
+
+hamlet = Piglet()
+hamlet.name = "hamlet"
+print(hamlet.speak())
+
+Petunia = Piglet()
+Petunia.name = "Petunia"
+print(Petunia.speak())
+
+
+################################################################
+
+
+class PigletAge:
+    years = 0
+    def pig_years(self):
+        return self.years * 18
+
+piggy = PigletAge()
+piggy.years = 2
+print(piggy.pig_years())
