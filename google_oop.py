0ec5730bf21c96b18bf6737e50dde4ab441c1865
amoako419 <ghheskey@gmail.com>
2024-09-18 04:17:04 +0000

    attritubes added

---
+++ google_oop.py
@@ -0,0 +1,12 @@
+# This is from a youtube class I took on OOP from google
+
+class Apple:
+    color = ""
+    flavor = ""
+    
+jonagold = Apple()
+jonagold.flavor = "sweet"
+jonagold.color = "red"
+
+print(jonagold.color.upper())
+print(jonagold.flavor.capitalize())
