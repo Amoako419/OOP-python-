d0101e5a5935bbdcf3768b3628bbf6298a4e70dc
amoako419 <ghheskey@gmail.com>
2024-09-23 04:23:07 +0000

    udemy class assignment done

---
+++ udemy1.py
@@ -0,0 +1,17 @@
+class Product:
+    def __init__ (self,Pname,Bname,Mname,Price):
+        # Product name = Pname
+        self.Pname = Pname
+        # Brand name = Bname
+        self.Bname = Bname
+        # model name = Mname
+        self.Mname = Mname
+        # Price
+        self.price = Price
+
+iphone_X = Product('iPhone_X','Apple','Phone','¢2100')
+print(iphone_X.Pname)
+print(iphone_X.Bname)
+print(iphone_X.Mname)
+print(iphone_X.price)
+        
